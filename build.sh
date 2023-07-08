8e5e3c8ff67d05c05d1786604676c64ff7490199
silver <silv3rr@users.noreply.github.com>
2023-07-08 12:19:35 +0200

    ci: add flask to binaries, fix python 3.9+ ver

--- build.sh
+++ build.sh
@@ -68,7 +68,7 @@ command -V python3 || {
 }
 #command -V bc || { echo "ERROR: bc not found"; exit 1; }
 
-PYVER="$(python3 --version | sed 's/.* \([0-9]\.[0-9]\).*/\1/' | grep -E '^[0-9.]+$' || echo 0)"
+PYVER="$(python3 --version | sed 's/.* \([0-9]\.[0-9]\{1,2\}\).*/\1/' | grep -E '^[0-9.]+$' || echo 0)"
 PYVER_OK=0
 if command -V bc >/dev/null 2>&1; then
   if [ "$(echo "$PYVER >= $PYREQVER" | bc)" -eq 1 ]; then
@@ -76,9 +76,9 @@ if command -V bc >/dev/null 2>&1; then
   fi
 else
   PYVER_MAY="$(echo "$PYVER" | sed 's/\([0-9]\)\.[0-9]/\1/')"
-  PYVER_MIN="$(echo "$PYVER" | sed 's/[0-9]\.\([0-9]\)/\1/')"
+  PYVER_MIN="$(echo "$PYVER" | sed 's/[0-9]\.\([0-9]\+\)/\1/')"
   PYREQVER_MAY="$(echo $PYREQVER | sed 's/\([0-9]\)\.[0-9]/\1/')"
-  PYREQVER_MIN="$(echo $PYREQVER | sed 's/[0-9]\.\([0-9]\)/\1/')"
+  PYREQVER_MIN="$(echo $PYREQVER | sed 's/[0-9]\.\([0-9]\+\)/\1/')"
   if [ "$PYVER_MAY" -gt "$PYREQVER_MAY" ]; then
     PYVER_OK=1
   elif [ "$PYVER_MAY" -eq "$PYREQVER_MAY" ] && [ "$PYVER_MIN" -ge "$PYREQVER_MIN" ]; then
@@ -93,8 +93,9 @@ fi
 
 ECNT=0
 for i in $REQS; do
+  PKG="$( echo $i | tr '_' '-' )"
   printf "%b\n" 'try:\n  import '"${i}"'\nexcept:\n  exit(1)' | python3 || {
-    echo "Module '${i}' not found, try 'apt install python3-${i}' or 'pip install ${i}'"
+    echo "Module '${i}' not found, try 'apt install python3-${PKG}' or 'pip install ${PKG}'"
     ECNT=$((ECNT + 1))
   }
 done
@@ -105,7 +106,7 @@ fi
 
 if [ "$PYINSTALLER" -eq 1 ]; then
   command -v pyinstaller >/dev/null 2>&1 || {
-    echo "ERROR: pyinstaller not found, try 'pip install pyinstaller'"
+    echo "ERROR: pyinstaller not found, try 'apt install python3-pyinstaller' or 'pip install pyinstaller'"
     exit 1
   }
   pyinstaller spy.py $ARGS --clean --noconfirm --onefile &&
