f5e564b67cb5c87c278f2b8e197d1f1663dee323
silver <silv3rr@users.noreply.github.com>
2024-07-05 23:01:51 +0200

    ci: remove centos, add rock/alma

--- docker-build-distros.sh
+++ docker-build-distros.sh
@@ -23,16 +23,16 @@ DEBIAN="
   pip3 install pyinstaller sysv-ipc geoip2 flask
   WEBSPY=0 ./build.sh && mkdir -p ./build-debian && mv -f *.tar.gz *.sha512sum ./build-debian
 "
-CENTOS="
-  yum install -y gcc python3-devel python3-pip python3-virtualenv
-  yum install -y https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/u/ucl-1.03-24.el7.x86_64.rpm
-  yum install -y https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/u/upx-3.96-9.el7.x86_64.rpm
-  python3 -m venv venv && . venv/bin/activate
-  pip3 install --upgrade pip wheel setuptools
-  pip3 install -r requirements.txt
-  pip3 install pyinstaller sysv-ipc geoip2
-  WEBSPY=0 ./build.sh  && mkdir -p ./build-centos && mv -f *.tar.gz *.sha512sum ./build-centos
-"
+#CENTOS="
+#  yum install -y gcc python3-devel python3-pip python3-virtualenv
+#  yum install -y https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/u/ucl-1.03-24.el7.x86_64.rpm
+#  yum install -y https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/u/upx-3.96-9.el7.x86_64.rpm
+#  python3 -m venv venv && . venv/bin/activate
+#  pip3 install --upgrade pip wheel setuptools
+#  pip3 install -r requirements.txt
+#  pip3 install pyinstaller sysv-ipc geoip2
+#  WEBSPY=0 ./build.sh  && mkdir -p ./build-centos && mv -f *.tar.gz *.sha512sum ./build-centos
+#"
 ALPINE="
   apk add python3 python3-dev py3-pip py3-virtualenv gcc musl-dev upx
   python3 -m venv venv && . venv/bin/activate
@@ -40,6 +40,16 @@ ALPINE="
   pip3 install -r requirements.txt
   pip3 install pyinstaller sysv-ipc geoip2 flask
   WEBSPY=0 ./build.sh && mkdir -p ./build-alpine && mv -f *.tar.gz *.sha512sum ./build-alpine
+"
+RHEL="
+  dnf install -y gcc python3-devel python3-pip
+  dnf install -y epel-release
+  dnf install -y upx ucl
+  python3 -m venv venv && . venv/bin/activate
+  pip3 install --upgrade pip wheel setuptools
+  pip3 install -r requirements.txt
+  pip3 install pyinstaller sysv-ipc geoip2
+  ./build.sh && mkdir ./build-alma && mv -f *.tar.gz *.sha512sum ./build-alma
 "
 
 func_docker_run() {
@@ -53,5 +63,6 @@ case $1 in
   debian) func_docker_run debian:12 "$DEBIAN" ;;
   centos) func_docker_run centos:7 "$CENTOS" ;;
   alpine) func_docker_run alpine:3.18 "$ALPINE" ;;
-  *) echo "ERROR: missing distro"; exit 1 ;;
+  alma) func_docker_run almalinux:9.4 "$RHEL" ;;
+  rocky) func_docker_run rockylinux:9.3 "$RHEL" ;;
 esac
