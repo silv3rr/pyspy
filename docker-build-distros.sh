4c3cc45c825cbc0b0ba0025c4583d47ce04847aa
silver <silv3rr@users.noreply.github.com>
2024-07-05 22:14:49 +0200

    build: all lnx distros (local)

---
+++ docker-build-distros.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# from github workflow
+
+UBUNTU="
+  python3 -m venv venv && . venv/bin/activate
+  pip3 install --upgrade pip wheel setuptools
+  pip3 install -r requirements.txt
+  pip3 install pyinstaller sysv-ipc geoip2 flask
+  WEBSPY=0 ./build.sh && mkdir -p ./build-ubuntu && mv -f *.tar.gz *.sha512sum ./build-ubuntu
+"
+DEBIAN="
+  DEBIAN_FRONTEND=noninteractive apt-get update -y;
+  apt-get install -y wget python3 python3-venv python3-pip
+  apt-get install -y upx-ucl || {
+    apt-get install -y libucl1 &&
+    wget -q http://ftp.us.debian.org/debian/pool/main/u/upx-ucl/upx-ucl_3.96-3+b1_amd64.deb &&
+    dpkg -i upx-ucl_3.96-3+b1_amd64.deb;
+  };
+  python3 -m venv venv && . venv/bin/activate
+  pip3 install --upgrade pip wheel setuptools
+  pip3 install -r requirements.txt
+  pip3 install pyinstaller sysv-ipc geoip2 flask
+  WEBSPY=0 ./build.sh && mkdir -p ./build-debian && mv -f *.tar.gz *.sha512sum ./build-debian
+"
+CENTOS="
+  yum install -y gcc python3-devel python3-pip python3-virtualenv
+  yum install -y https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/u/ucl-1.03-24.el7.x86_64.rpm
+  yum install -y https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/u/upx-3.96-9.el7.x86_64.rpm
+  python3 -m venv venv && . venv/bin/activate
+  pip3 install --upgrade pip wheel setuptools
+  pip3 install -r requirements.txt
+  pip3 install pyinstaller sysv-ipc geoip2
+  WEBSPY=0 ./build.sh  && mkdir -p ./build-centos && mv -f *.tar.gz *.sha512sum ./build-centos
+"
+ALPINE="
+  apk add python3 python3-dev py3-pip py3-virtualenv gcc musl-dev upx
+  python3 -m venv venv && . venv/bin/activate
+  pip3 install --upgrade pip wheel setuptools
+  pip3 install -r requirements.txt
+  pip3 install pyinstaller sysv-ipc geoip2 flask
+  WEBSPY=0 ./build.sh && mkdir -p ./build-alpine && mv -f *.tar.gz *.sha512sum ./build-alpine
+"
+
+func_docker_run() {
+  image=$1
+  shift
+  docker run -it --rm  --workdir /build -v $PWD:/build $image sh -c "$*"
+}
+
+case $1 in
+  ubuntu) func_docker_run ubuntu24.04 "$UBUNTU" ;;
+  debian) func_docker_run debian:12 "$DEBIAN" ;;
+  centos) func_docker_run centos:7 "$CENTOS" ;;
+  alpine) func_docker_run alpine:3.18 "$ALPINE" ;;
+  *) echo "ERROR: missing distro"; exit 1 ;;
+esac
